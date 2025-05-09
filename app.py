b34b541a5827999e0ec8faba2a6451812e186683
Kablys <1136529+Kablys@users.noreply.github.com>
2025-05-09 16:06:56 +0300

    debug to prod

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def upload():
     return render_template('index.html', result_url=result_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
